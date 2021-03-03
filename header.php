2e04324517b68b1979a523c17804f282f02f0cf4
pablocazorla <pablo.david.cazorla@gmail.com>
2021-03-03 10:40:33 -0300

    Implement Google tag manager

--- header.php
+++ header.php
@@ -17,11 +17,38 @@
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Open+Sans:wght@400;700&display=swap"
     rel="stylesheet">
 
+  <!-- Google Tag Manager -->
+
+  <?php
+    $GTM = 'GTM-T33BZF3';
+  ?>
+
+  <script>
+  (function(w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({
+      'gtm.start': new Date().getTime(),
+      event: 'gtm.js'
+    });
+    var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s),
+      dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src =
+      'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+  })(window, document, 'script', 'dataLayer', <?php echo $GTM; ?>);
+  </script>
+  <!-- End Google Tag Manager -->
+
   <?php wp_head(); ?>
 </head>
 
 <body>
-
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?php echo $GTM; ?>" height="0" width="0"
+      style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
   <div class="main-wrapper">
     <header class="main-header" id="main-header" data-aos="fade-down">
       <div class="container">
